2361b1cf17c7cddb51f1e20141fabb0d491a118e
Bubu-Droid <fufudadw@gmail.com>
2025-07-07 18:57:26 +0530

    Update testsite

--- testsite/website/settings.py
+++ testsite/website/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-goz+ckz4y8sne+kc#x7th*7zft6$wilpzbpy@2n=wsp@^4yi$%"
+from website.rc import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
